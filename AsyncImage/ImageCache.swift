2e44eafe40d68a041e8e73eb1972d1e2a1f21e24
V8tr <ne0_n@mail.ru>
2021-01-08 19:01:59 +0200

    Set cache limit

--- AsyncImage/ImageCache.swift
+++ AsyncImage/ImageCache.swift
@@ -13,7 +13,12 @@ protocol ImageCache {
 }
 
 struct TemporaryImageCache: ImageCache {
-    private let cache = NSCache<NSURL, UIImage>()
+    private let cache: NSCache<NSURL, UIImage> = {
+        let cache = NSCache<NSURL, UIImage>()
+        cache.countLimit = 100 // 100 items
+        cache.totalCostLimit = 1024 * 1024 * 100 // 100 MB
+        return cache
+    }()
     
     subscript(_ key: URL) -> UIImage? {
         get { cache.object(forKey: key as NSURL) }
